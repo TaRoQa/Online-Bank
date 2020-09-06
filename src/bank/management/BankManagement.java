c529eaf6577c1258f0c0ac2fff400ddda5f7ab15
ShaimaaHassan3 <70854824+ShaimaaHassan3@users.noreply.github.com>
2020-09-06 19:52:15 +0200

    Online-Bank

--- src/bank/management/BankManagement.java
+++ src/bank/management/BankManagement.java
@@ -6,10 +6,12 @@
 package bank.management;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -33,5 +35,162 @@ public class BankManagement extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+  
+ public class Person
+  {
+private String Name,Emai,Gender,Bithdate,Address,Password;
+        public Person()
+        {
+        }
+        public void setName(String Name) {
+            this.Name = Name;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public String getEmai() {
+            return Emai;
+        }
+
+        public String getGender() {
+            return Gender;
+        }
+
+        public String getBithdate() {
+            return Bithdate;
+        }
+
+        public String getAddress() {
+            return Address;
+        }
+
+        public String getPassword() {
+            return Password;
+        }
+
+        public void setEmai(String Emai) {
+            this.Emai = Emai;
+        }
+
+        public void setGender(String Gender) {
+            this.Gender = Gender;
+        }
+
+        public void setBithdate(String Bithdate) {
+            this.Bithdate = Bithdate;
+        }
+
+        public void setAddress(String Address) {
+            this.Address = Address;
+        }
+
+        public void setPassword(String Password) {
+            this.Password = Password;
+        }
+
+        public Person(String Name, String Emai, String Gender, String Bithdate, String Address, String Password) {
+            this.Name = Name;
+            this.Emai = Emai;
+            this.Gender = Gender;
+            this.Bithdate = Bithdate;
+            this.Address = Address;
+            this.Password = Password;
+        }
+     public void Display()
+     {
+         JOptionPane.showMessageDialog(null,"Name "+getName()+"\nEmail "+getEmai()
+                 +"\nGender "+getGender()+"\nPassword "+getPassword());
+     }
+     
+  }
+ class Bank
+ {
+     private String ID,Location,Phone;
+     private  float Rate;
+     private Person Manager;
+
+        public Bank(String ID, String Location, String Phone, float Rate, Person Manager) {
+            this.ID = ID;
+            this.Location = Location;
+            this.Phone = Phone;
+            this.Rate = Rate;
+            this.Manager = Manager;
+        }
+
+        public String getID() {
+            return ID;
+        }
+
+        public String getLocation() {
+            return Location;
+        }
+
+        public String getPhone() {
+            return Phone;
+        }
+
+        public float getRate() {
+            return Rate;
+        }
+
+        public Person getManager() {
+            return Manager;
+        }
+
+        public void setID(String ID) {
+            this.ID = ID;
+        }
+
+        public void setLocation(String Location) {
+            this.Location = Location;
+        }
+
+        public void setPhone(String Phone) {
+            this.Phone = Phone;
+        }
+
+        public void setRate(float Rate) {
+            this.Rate = Rate;
+        }
+
+        public void setManager(Person Manager) {
+            this.Manager = Manager;
+        }
+     
+ }
+ class Admain extends Person
+ {
+
+        public Admain() {
+        }
+
+        public Admain(String Name, String Emai, String Gender, String Bithdate, String Address, String Password) {
+            super(Name, Emai, Gender, Bithdate, Address, Password);
+        }
+     
+ }
+ class Client extends Person
+ {
+
+        public Client() {
+        }
+
+        public Client(String Name, String Emai, String Gender, String Bithdate, String Address, String Password) {
+            super(Name, Emai, Gender, Bithdate, Address, Password);
+        }
+     
+ }
+ class Employee extends Person
+ {
+
+        public Employee() {
+        }
+
+        public Employee(String Name, String Emai, String Gender, String Bithdate, String Address, String Password) {
+            super(Name, Emai, Gender, Bithdate, Address, Password);
+        }
+     
+ }
 }
--- src/bank/management/FXMLDocumentController.java
+++ src/bank/management/FXMLDocumentController.java
@@ -5,25 +5,36 @@
  */
 package bank.management;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
+import com.sun.deploy.util.SessionState.Client;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Tareq
  */
 public class FXMLDocumentController implements Initializable {
-    
+    BankManagement  bm=new BankManagement();
     @FXML
     private Label label;
     @FXML
@@ -34,7 +45,10 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private JFXTextField email;
-
+    
+    @FXML
+    private JFXTextField repass;
+    
     @FXML
     private JFXTextField password;
 
@@ -49,20 +63,70 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private JFXDatePicker birthdate;
     public ObservableList<String>list_gender=FXCollections.observableArrayList("Male","Female");
-    public ObservableList<String>list_status=FXCollections.observableArrayList("Admin","Employee","Client");
+    public ObservableList<String>list_status=FXCollections.observableArrayList("Admain","Employee","Client");
+
+    public FXMLDocumentController() {
+        
+    }
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
+    
     @FXML
-    void Register(ActionEvent event) {
-
+    void Register(ActionEvent event)
+    {
+        String Emial =email.getText();
+        if(!password.getText().equals(repass.getText()))
+            JOptionPane.showMessageDialog(null,"Two Password Not Matc");
+        else if(!isValid(Emial)) 
+           email.setStyle("-fx-border-color: red "); 
+        else{
+    String selectedChoice = (String) status.getSelectionModel().getSelectedItem();
+    String selectedChoicegender = (String) gender.getSelectionModel().getSelectedItem();
+     try{
+         email.setStyle("-fx-border-color: white "); 
+     if(selectedChoice.equals("Admain"))
+     {
+         BankManagement.Person person=bm.new Admain (firstname.getText()+" "+lastname.getText(),email.getText()
+                 ,selectedChoicegender,"","",password.getText());
+         person.Display();
+     }  
+     else if(selectedChoice.equals("Employee"))
+     {
+         BankManagement.Person person=bm.new Employee(firstname.getText()+" "+lastname.getText(),email.getText()
+                 ,selectedChoicegender,"","",password.getText());;
+         person.Display();
+     }
+     else if(selectedChoice.equals("Client"))
+     {
+        BankManagement.Person person=bm.new Client(firstname.getText()+" "+lastname.getText(),email.getText()
+                ,selectedChoicegender,"","",password.getText());;
+         person.Display();
+     }}catch(Exception exception)
+     {
+         JOptionPane.showMessageDialog(null,"You  Status OR Gender Not Selected");
+     }}
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gender.setItems(list_gender);
         status.setItems(list_status);
-    }    
     
+    }    
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        Matcher matcher=pat.matcher(email);
+        if (matcher.matches())
+            return true;
+        else 
+            return false;
+    }
 }
